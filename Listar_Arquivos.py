34000468f3066176a8049df094246ce403fed5ce
LucasDonato333 <41838810+LucasDonato333@users.noreply.github.com>
2019-07-29 16:58:34 -0300

    Delete Listar_Arquivos.py

--- Listar_Arquivos.py
+++
@@ -1,5 +0,0 @@
-import glob
-listar_arquivo = glob.glob('*.*')# PROCURA NO DIRETORIO TODO ARQUIVO QUE TENHA EXTENSÃO "xlsx".
-with open("Lista_Nome_Arquivos.txt","w") as f:
-    for linha in listar_arquivo:
-        f.write(linha+"\n")
